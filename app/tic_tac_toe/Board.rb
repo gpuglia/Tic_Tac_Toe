52632ad918afb9f0e5d5d43240d2e299be9cbde1
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-05 17:58:39 -0430

    Adds Board class

---
+++ app/tic_tac_toe/Board.rb
@@ -0,0 +1,37 @@
+module Tic_Tac_Toe
+  class Board
+    def initialize(array)
+      self.board = array
+    end
+
+    def rows
+      @board
+    end
+
+    def columns
+      @board.transpose
+    end
+
+    def diagonals
+      diagonal_1 = []  
+      diagonal_2 = []  
+
+      @board.each_with_index do |row, i|
+        diagonal_1 << row[i]
+        diagonal_2 << row[2 - i]
+      end
+
+      return [diagonal_1, diagonal_2]
+    end
+
+    def lines
+      rows + columns + diagonals
+    end
+
+    private
+
+    def board=(array)
+      @board = array.each_slice(3).to_a
+    end
+  end
+end
---
+++ spec/tic_tac_toe/board_spec.rb
@@ -0,0 +1,5 @@
+describe 'Tic_Tac_Toe' do
+  describe 'Board' do
+    
+  end
+end
