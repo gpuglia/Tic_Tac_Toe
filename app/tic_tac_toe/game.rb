e1ada0d94be5de669af5264ed8c686576d71280e
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-05 01:22:37 -0430

    completely modifies game class, deletes user_spec that came in skeleton

--- app/tic_tac_toe/game.rb
+++ app/tic_tac_toe/game.rb
@@ -10,11 +10,43 @@ module Tic_Tac_Toe
 
     def win?(player)
       mark = mark(player)
-      check_rows_columns(mark) || check_diagonals(mark)
+      lines.each { |line| return true if complete_line?(line, mark) }
+      return false
     end
 
+    def lines
+      rows = @board
+      columns = @board.transpose
+      rows + columns + diagonals
+    end
+
+    def complete_line?(line, mark)
+        line.each do |elem|
+          return false unless elem == mark
+        end
+        return true
+    end
+    
+    def diagonals
+      diagonal_1 = []  
+      diagonal_2 = []  
+
+      @board.each_with_index do |row, i|
+        diagonal_1 << row[i]
+        diagonal_2 << row[2 - i]
+      end
+
+      return [diagonal_1, diagonal_2]
+    end
+
+    def mark(player)
+      player == :human ? "X" : "O"
+    end
+
+    private
+
     def board=(board)
-      @board = board.each_slice.to_a
+      @board = board.each_slice(3).to_a
     end
   end
 end
--- spec/user_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe User do
-  it "validates email" do
-    should respond_to(:valid?)
-  end
-end
