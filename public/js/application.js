5b0173b5b7ca7b8b68d991f6feec78393e09a2ba
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-07 02:59:47 -0430

    modifies read method in application.js, removes some unecessary files

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,30 @@
-// Setup AngularJS controllers here. //
+var Board = function() {
+
+};
+
+
+Board.prototype.read = function() {
+  boardArray = [];
+  $('#board td').map(function() {
+    boardArray.push($(this).html());    
+  });
+
+  return boardArray;
+}
+
+
+// Board.prototype.write(move, mark) = 
+
+$(document).ready(function() {
+  board = new Board;
+
+  $('table').on('click', 'td', function() {
+    console.log($(this).html());
+    if($(this).html() == "") {
+      $(this).html('X') 
+      getComputerMove(board.read())
+    }
+  })
+})
+
+
