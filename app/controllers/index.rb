f6cba3bc68c6b7ce4f63456ea819d486b5946d87
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-07 03:13:50 -0430

    adds controller logic for handling the request for a computer move

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,33 +1,10 @@
 get '/' do
-	#These are params if there was an error (otherwise nil)
-	@login_error = params[:params_login]
-	@register_error = params[:params_registration]
+	# computer = Tic_Tac_Toe::Ai.new
   erb :index
 end
 
-post '/login' do
-	email = params[:email]
-	password = params[:password]
-	if User.authenticate(email, password)
-		user = User.find_by_email(email)
-		session[:user_id] = (User.find_by_email(email)).id
-		redirect '/home'
-	else
-		redirect '/?params_login=There was an error with login.'
-	end
-
-end
-
-post '/register' do
-	email = params[:email]
-	password = params[:password]
-	user = User.new(email: email, password: password)
-	if !user.valid?
-		redirect to '/?params_registration=There was an error with registration'
-	else
-		user.save
-
-		session[:user_id] = @user.id
-		redirect '/home'
-	end
+post '/computer_move' do
+  board = Tic_Tac_Toe::Board.new(params[:board])
+  game = Tic_Tac_Toe::Game.new(board)
+  Tic_Tac_Toe::Ai.find_best_move(game).to_json
 end
