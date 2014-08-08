220f781631921635e13935520a66ee79aa9b50b6
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-08 15:29:24 -0430

    refactors ai tests

--- spec/tic_tac_toe/ai_spec.rb
+++ spec/tic_tac_toe/ai_spec.rb
@@ -6,27 +6,21 @@ describe 'Tic_Tac_Toe' do
     describe '#find_best_move' do
       context 'when there is an immediate possibility of winning' do
         it 'returns the move that wins in the immediate turn' do
-          board = Tic_Tac_Toe::Board.new(["X", "", "O", "", "O", "X", "", "X", ""])
-          game = Tic_Tac_Toe::Game.new(board)
-
+          game = Tic_Tac_Toe::Game.new(["X", "", "O", "", "O", "X", "", "X", ""])
           expect(Tic_Tac_Toe::Ai.find_best_move(game)).to eq({:move=>7, :rating=>100})
         end
       end
 
       context 'when there is an immediate possibility of losing' do
         it "returns the move that blocks the opponent's win" do
-          board = Tic_Tac_Toe::Board.new(["X", "X", "", "", "O", "", "", "", ""])
-          game = Tic_Tac_Toe::Game.new(board)
-
+          game = Tic_Tac_Toe::Game.new(["X", "X", "", "", "O", "", "", "", ""])
           expect(Tic_Tac_Toe::Ai.find_best_move(game)[:move]).to eq(3)
         end
       end
 
       context 'when there is an immediate chance of winning or loosing' do
         it 'returns the move that wins in the immediate turn' do
-          board = Tic_Tac_Toe::Board.new(["X", "", "X", "", "O", "O", "", "", "X"])
-          game = Tic_Tac_Toe::Game.new(board)
-
+          game = Tic_Tac_Toe::Game.new(["X", "", "X", "", "O", "O", "", "", "X"])
           expect(Tic_Tac_Toe::Ai.find_best_move(game)).to eq({:move=>4, :rating=>100})
         end
       end
