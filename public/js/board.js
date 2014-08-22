f4bd37ccc94b8cb1eb74a0895b4429db31cf6355
Gustavo Puglia <gpuglia88@gmail.com>
2014-08-22 02:02:27 -0430

    moves board class to board.js

---
+++ public/js/board.js
@@ -0,0 +1,35 @@
+var Board = function() {
+  this.locked = true;
+  this.cells = $('table td');
+};
+
+Board.prototype.read = function() {
+  var boardArray = [];
+  this.cells.map(function() {
+    boardArray.push($(this).html());    
+  });
+
+  return boardArray;
+}
+
+Board.prototype.lock = function() {
+  this.locked = true;
+}
+
+Board.prototype.unlock = function() {
+  this.locked = false;
+}
+
+Board.prototype.write = function(cell, mark) {
+  cell.html(mark);
+}
+
+Board.prototype.getCellByMove = function(move) {
+    var row = parseInt((move - 1) / 3) + 1;
+    var column = ((move - 1) % 3) + 1;
+    return $('table tr:nth-child(' + row + ') td:nth-child(' + column + ')')
+}
+
+Board.prototype.reset = function() {
+  this.cells.html('');
+}
